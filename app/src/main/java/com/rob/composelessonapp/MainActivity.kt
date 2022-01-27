977027a4977a6fe21cf7cad646d06571e3b01cc0
Rob <robgas45@gmail.com>
2022-01-27 21:24:53 +0400

    8-commit

--- app/src/main/java/com/rob/composelessonapp/MainActivity.kt
+++ app/src/main/java/com/rob/composelessonapp/MainActivity.kt
@@ -8,18 +8,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.room.Room
 import com.rob.composelessonapp.Button.UsingCustomButton_4
 import com.rob.composelessonapp.Layout.BoxScreen
-import com.rob.composelessonapp.ROOM.AppDatabase
-import com.rob.composelessonapp.ROOM.Employer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val database = Room.databaseBuilder(            // create database
-            applicationContext,
-            AppDatabase::class.java, "EmployerDB.db"
-        ).build()
-        val employerDao = database.employerDAO()    // create object of DAO class
-        val emp: List<Employer> = employerDao.getAll()  // use method of DAO class
 
         setContent {
 //            TextScreen(FontWeight.ExtraBold)
--- app/src/main/java/com/rob/composelessonapp/ROOM/DatabaseClass.kt
+++
@@ -1,9 +0,0 @@
-package com.rob.composelessonapp.ROOM
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = arrayOf(Employer::class, Education::class), version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun employerDAO(): EmployerDAO
-}
